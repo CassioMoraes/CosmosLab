b61a4baec81eb2cd03ea3411a046603825b19d79
CassioMoraes <cassio.castilhos@gmail.com>
2016-03-06 18:02:27 -0300

    Sparkling completo

--- js/sparkle-spawn.js
+++ js/sparkle-spawn.js
@@ -3,21 +3,35 @@ var tickTimer = 20;
 setInterval(onTimerTick, tickTimer);
 
 function StartSparkling() {
+    var sparkleMaxSize = 10;
+    var sparkleMinSize = 5;
+    
     for (i = 0; i < 100; i++) {
         
-        var posx = (Math.random() * ($(document).width() - 8)).toFixed();
-        var posy = (Math.random() * ($(document).height() - 8)).toFixed();
+        var x = (Math.random() * ($(document).width() -  sparkleMaxSize)).toFixed();
+        var y = (Math.random() * ($(document).height() - sparkleMaxSize)).toFixed();
+        var randonSize = Math.floor(Math.random() * sparkleMaxSize) + sparkleMinSize;
 
-        var $circle = $("<div>", {class: "circle pulsate"});
-        $circle.css({
-            'left':posx+'px',
-            'top':posy+'px'});
-        $(".container").append($circle);
+        var $circle = $("<div>", { id:"circle" + i, class: "circle pulsate"});               
+        $circle.css(
+            {
+                'left': x + 'px',
+                'top': y + 'px',
+                'width': randonSize + 'px',
+                'height': randonSize + 'px',                
+                '-webkit-border-radius': randonSize / 2 + 'px',
+                '-moz-border-radius':  randonSize / 2 + 'px',
+                'border-radius':  randonSize / 2 + 'px'
+            });
+            
+        $(".sparkles").append($circle);
     }
 }
 
 function onTimerTick() {
-    $(".pulsate").animate({ opacity: 0.2 }, 1200, 'linear')
-            .animate({ opacity: 1 }, 1200, 'linear', pulsate)
-            .animate({ opacity: 1 }, 400, 'linear', pulsate);    
+    var randomSplarkle = Math.floor(Math.random() * 100) + 1;
+    
+    $("#circle" + randomSplarkle).animate({ opacity: 0.2 }, 1200, 'linear')    
+            .animate({ opacity: 1 }, 1200, 'linear')
+            .animate({ opacity: 1 }, 400, 'linear');    
 }
