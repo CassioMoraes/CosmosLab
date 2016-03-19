31ac62aa1481b07dd18a1d48c335b13733da6608
CassioMoraes <cassio.castilhos@gmail.com>
2016-03-19 18:17:57 -0300

    Corrigido menu para não scrollar mais para a pagina inicial

--- js/overlay.js
+++ js/overlay.js
@@ -1,10 +1,25 @@
+var offsetY;
+
 function toggleNav() {
-    if (document.getElementById("options").style.height == "100%") {
-        document.getElementById("options").style.height = "0%";
+    if (document.getElementById("options").style.opacity == "1") {
+        document.getElementById("options").style.opacity = "0";
         $("#menu").attr("src", "img/Icon_OpenMenu.png");
     }
     else {
-        document.getElementById("options").style.height = "100%";
+        document.getElementById("options").style.opacity = "1";
         $("#menu").attr("src", "img/Icon_CloseMenu.png");
     }
 }
+
+// function toggleNav() {
+//     if (document.getElementById("options").style.height == "100%") {
+//         document.getElementById("options").style.height = "0%";
+//         $("#menu").attr("src", "img/Icon_OpenMenu.png");
+//         close();
+//     }
+//     else {
+//         document.getElementById("options").style.height = "100%";
+//         $("#menu").attr("src", "img/Icon_CloseMenu.png");
+//         open();
+//     }
+// }
