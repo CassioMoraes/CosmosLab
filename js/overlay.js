5fa66116c89f6b144c1eddd7428c7f2b11149e4d
CassioMoraes <cassio.castilhos@gmail.com>
2016-03-27 18:22:53 -0300

    Criado pagina de contato e resolvido bug de overlay

--- js/overlay.js
+++ js/overlay.js
@@ -1,25 +1,16 @@
-var offsetY;
+window.onload = function() {
+    document.getElementById("options").style.pointerEvents = "none";
+};
 
 function toggleNav() {
     if (document.getElementById("options").style.opacity == "1") {
+        document.getElementById("options").style.pointerEvents = "none";
         document.getElementById("options").style.opacity = "0";
         $("#menu").attr("src", "img/Icon_OpenMenu.png");
     }
     else {
+        document.getElementById("options").style.pointerEvents = "auto";
         document.getElementById("options").style.opacity = "1";
         $("#menu").attr("src", "img/Icon_CloseMenu.png");
     }
 }
-
-// function toggleNav() {
-//     if (document.getElementById("options").style.height == "100%") {
-//         document.getElementById("options").style.height = "0%";
-//         $("#menu").attr("src", "img/Icon_OpenMenu.png");
-//         close();
-//     }
-//     else {
-//         document.getElementById("options").style.height = "100%";
-//         $("#menu").attr("src", "img/Icon_CloseMenu.png");
-//         open();
-//     }
-// }
